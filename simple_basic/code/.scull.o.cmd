d780505cc99ba3ad21f6a3a618121fd9af3b16be
lanru <17381792@qq.com>
2023-04-11 17:14:10 +0800

    delete unused code

--- simple_basic/code/.scull.o.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/hacker/git/Linux_Scull/simple_basic/code/scull.o := ld -m elf_x86_64   -r -o /home/hacker/git/Linux_Scull/simple_basic/code/scull.o /home/hacker/git/Linux_Scull/simple_basic/code/main.o 
